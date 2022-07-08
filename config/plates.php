00d8895fbd83bace8216b0e8cf153b153a7b018d
riccardoferro <riccardo.ferron8@gmail.com>
2022-07-08 18:29:02 +0200

    Iniziati seeder

---
+++ config/plates.php
@@ -0,0 +1,67 @@
+
+
+<?php
+
+return [
+    [
+      "name" => "Fast Food",
+      "image" => "fast-food.jpg",
+    ],
+    [
+      "name" => "Pizza",
+      "image" => "pizza-category.jpg",
+    ],
+    [
+      "name" => "Cinese",
+      "image" => "chinese-category.jpg",
+    ],
+    [
+      "name" => "Giapponese",
+      "image" => "sushi-category.jpg",
+    ],
+    [
+      "name" => "Poke",
+      "image" => "poke-category.jpg",
+    ],
+    [
+      "name" => "Dessert",
+      "image" => "dessert-category.jpg",
+    ],
+
+    [
+      "name" => "Tailandese",
+      "image" => "thailandese-category.jpeg",
+    ],
+
+    [
+      "name" => "Italiano",
+      "image" => "italiano-category.jpeg",
+    ],
+
+    [
+      "name" => "Smoothie",
+      "image" => "smoothie-category.jpeg",
+    ],
+
+    [
+      "name" => "Gelato",
+      "image" => "gelato-category.webp",
+    ],
+
+    [
+      "name" => "Vegano",
+      "image" => "vegan-category.webp",
+    ],
+
+    [
+      "name" => "Gluten Free",
+      "image" => "gluten-free-category.jpeg",
+    ],
+
+    [
+      "name" => "Vegetariano",
+      "image" => "vegetariano-category.jpeg",
+    ],
+      
+
+];
